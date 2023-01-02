4dcc6504402e8c1f6f9c6570b1706db01bd19cd0
olaniyanp11 <olaniyanp11@gmail.com>
2023-01-02 01:52:06 +0100

    first branch

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>
+
+<i>
+this project is the first webpage in 2023
+
+</i>
+</h1>
+<h3> 
+the aim of january 2023 is to build 15 website and this just number one
+</h3>
+
---
+++ java.js
@@ -0,0 +1,6 @@
+function btnFunc() {
+    var btnn = document.getElementById("sec_1");
+    var bar = document.querySelector(".bar")
+    btnn.classList.toggle("out");
+    bar.classList.toggle("out");
+}
